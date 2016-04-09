f8177da6c6c2ba8657f6d6ca2a9b141f8aeb5e00
thaden123 <thaden123@gmail.com>
2016-04-09 15:03:46 -0400

    3pm Commit

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var index = require('./routes/index');
+var login = require('./routes/login');
 var critical = require('./routes/critical');
 
 var app = express();
@@ -23,7 +24,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', login);
+app.use('/index', index);
 app.use('/critical', critical);
 
 // catch 404 and forward to error handler
--- routes/login.js
+++ routes/login.js
@@ -7,3 +7,38 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+
+
+var options = {
+    clientUserId: encodeURIComponent('UNIQUE_ID_FOR_YOUR_USER'),
+    clientId: 'CLIENT_ID', // grab it from app settings page
+    publicToken: '',  // Leave blank for new users
+
+    finish: function(err, sessionTokenObject) {
+        /* Called after user finishes connecting their health data
+         You need to post `sessionTokenObject` to your server to then:
+         1. Append your `clientSecret` to it
+         2. Send send it to our server for user credentials
+
+         Sending a POST request with jQuery might look like this
+         (it's not necessary to use jQuery):
+         */
+        $.post('/your-servers-endpoint', sessionTokenObject, function(res){
+
+        });
+
+        // Include code here to refresh the page.
+
+    },
+    close: function() {
+        /* Optional callback called when a user closes the popup
+         without connecting any data sources */
+    },
+    error: function(err) {
+        /* Optional callback called if an error occurs when loading
+         the popup. */
+
+        // `err` has fields: `code`, `message`, `detailedMessage`
+    }
+}
