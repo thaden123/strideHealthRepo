c03b3fcee1f65161f4ac6f74dfa94070a2cf50d1
thaden123 <thaden123@gmail.com>
2016-04-09 13:56:03 -0400

    remove .html

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/', critical);
+app.use('/critical', critical);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
